d7d300874c37d54ea1f556e33a98663c2aec1e1b
Suzuki Shigeyuki <kyuuritosuika@gmail.com>
2023-09-09 16:30:36 +0900

    OFF tls version print

--- telnetsClient.py
+++ telnetsClient.py
@@ -12,7 +12,7 @@ context = ssl.create_default_context()
 
 with socket.create_connection((hostname, port)) as sock:
 	with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-		print(ssock.version())
+		#print(ssock.version())
 		while True:
 			response = ssock.recv(4096)
 			print(response.decode('UTF8'), end='')
