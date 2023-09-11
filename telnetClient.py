6a313375d960a7b622a061ac08034c69e55e1f23
Suzuki Shigeyuki <kyuuritosuika@gmail.com>
2023-09-11 10:33:49 +0900

    add option parse and help print

--- telnetClient.py
+++ telnetClient.py
@@ -7,28 +7,50 @@ import ssl
 import sys
 import threading
 
-codec = 'cp932'
-#codec = 'SJIS'
+def printHelp():
+	print("""telnetClient [-v] [-s] [-c codec] [-h] <host> [<port>]
+-s: telnets mode
+-c <codec>: use connecting with specified codec
+-v: verbose print
+-h: print this help
+""")
+	exit(1)
 
-optlist, args = getopt.getopt(sys.argv[1:], 's')
+optlist, args = getopt.gnu_getopt(sys.argv[1:], 'c:svh')
+
+if len(args) == 0:
+	printHelp()
 
 targetHost = args[0]
+
 #targetDomainNameOrIpAddr = args[0]
 #targetDomainNameOrIpAddr = sys.argv[1]
 #print("sys.argv : " + str(sys.argv))
 #targetDomainName = 'koukoku.shadan.open.ad.jp'
+
+# default value
+codec = 'cp932'
+#codec = 'SJIS'
 targetPort = 23
 if len(args) > 2 and args[1]:
 	targetPort = args[1]
 #if len(sys.argv) > 2 and sys.argv[2]:
 #	targetPort = sys.argv[2]
-
 telnetsMode = False
+def printVerbose(*message, file=sys.stderr):
+	pass
+
 for opt, arg in optlist:
-	if opt == '-s':
+	if opt == '-c':
+		codec = arg
+	elif opt == '-s':
 		telnetsMode = True
 		targetPort = 992
 		codec = 'UTF8'
+	elif opt == '-v':
+		printVerbose = print
+	elif opt == '-h':
+		printHelp()
 		
 #ipv4regex = r"((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
 #if not re.match(ipv4regex, targetDomainNameOrIpAddr):
@@ -43,7 +65,7 @@ for opt, arg in optlist:
 # generate TCP socket
 #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(targetHost, targetPort, socket.getservbyport(targetPort, 'tcp'))
+printVerbose(targetHost, targetPort, socket.getservbyport(targetPort, 'tcp'))
 # connect to target host
 #client.connect((targetHost, targetPort))
 client = socket.create_connection((targetHost, targetPort))
@@ -67,12 +89,13 @@ def receive(client):
 				print("UnicodeDecodeError", response.decode(codec))
 
 try:
+	telnetClient = None
 	if telnetsMode == True:
 		# start tls
 		context = ssl.create_default_context()
 		telnetClient = client
 		client = context.wrap_socket(client, server_hostname=targetHost)
-	receiveThread = threading.Thread(target=receive, args=(client, ))
+	receiveThread = threading.Thread(target=receive, args=(client, ), daemon=True)
 	receiveThread.start()
 	while True:
 		message = input("")
